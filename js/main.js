0442aba6e160cc4d9d593fb03a97e94b9d7edbc1
Michael <tsurkanmixail@gmail.com>
2021-12-14 12:05:52 +0200

    first try

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(function(){
+
+// });
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      loop:true,
+      autoplay: true,
+      items:1,
+      nav: true
+    });
+  });
